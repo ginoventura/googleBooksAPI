066b2086bd11f84778dd2abeb5df16688238b9f6
Gino Ventura <96146281+ginoventura@users.noreply.github.com>
2022-10-24 20:29:02 +0000

    first commit

---
+++ books.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+result = requests.get("https://www.googleapis.com/books/v1/volumes?q=uml+gota+a+gota")
+
+book = result.json()
+#print("Claves")
+#print(book.keys())
+
+items = book["items"]
+
+encoded = json.dumps(items)
+decoded = json.loads(encoded)
+
+#Informacion del elemento n de la busqueda
+#print(decoded[0])
+
+#Informacion del volumen
+#print(decoded[0]["volumeInfo"])
+
+#Sólo el titulo
+print(decoded[0]["volumeInfo"]["title"])
+print(decoded[1]["volumeInfo"]["title"])
+print(decoded[2]["volumeInfo"]["title"])
+print(decoded[3]["volumeInfo"]["title"])
+print(decoded[4]["volumeInfo"]["title"])
+
+#Enlace al libro
+#print(decoded[0]["volumeInfo"]["infoLink"])
+
+#print("Información del libro: ")
+#print(result.status_code)
+#print(result.text)
---
+++ mvc/index.py
@@ -0,0 +1,34 @@
+import web
+import requests 
+import json
+
+render = web.template.render("mvc/")
+
+class Index():
+  def GET(self):
+    return render.index()
+
+  def POST(self):
+    form = web.input()
+    book_name = form.book_name
+
+    result = requests.get("https://www.googleapis.com/books/v1/volumes?q="+book_name)
+
+    book = result.json()
+
+    items = book["items"]
+
+    encoded = json.dumps(items)
+    decoded = json.loads(encoded)
+
+    url = decoded[0]["volumeInfo"]["infoLink"]
+
+    link = "<a target='blank' href='"+url+"'>"+book_name+"</a>"
+
+    return link
+
+    #print(decoded[0]["volumeInfo"]["title"])
+    #print(decoded[1]["volumeInfo"]["title"])
+    #print(decoded[2]["volumeInfo"]["title"])
+    #print(decoded[3]["volumeInfo"]["title"])
+    #print(decoded[4]["volumeInfo"]["title"])
