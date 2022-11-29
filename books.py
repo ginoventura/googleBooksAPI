f82ead1ed4b528f62256e868551f6c743796e666
Gino Ventura <96146281+ginoventura@users.noreply.github.com>
2022-11-29 20:22:51 +0000

    final commit

--- books.py
+++ books.py
@@ -1,7 +1,14 @@
 import requests
 import json
 
-result = requests.get("https://www.googleapis.com/books/v1/volumes?q=uml+gota+a+gota")
+# 1) Libros de un tema especifico, sin filtros
+#result = requests.get("https://www.googleapis.com/books/v1/volumes?q=java")
+
+# 2) Libros disponibles electronicamente
+result = requests.get("https://www.googleapis.com/books/v1/volumes?q=html&download=epub")
+
+# 3) Libros gratis y disponibles electronicamente
+#result = requests.get("https://www.googleapis.com/books/v1/volumes?q=bases+de+datos&filter=free-ebooks&download=epub")
 
 book = result.json()
 #print("Claves")
@@ -12,18 +19,37 @@ items = book["items"]
 encoded = json.dumps(items)
 decoded = json.loads(encoded)
 
-#Informacion del elemento n de la busqueda
-#print(decoded[0])
-
-#Informacion del volumen
-#print(decoded[0]["volumeInfo"])
-
-#Sólo el titulo
+# ----------------------------------------------
 #print(decoded[0]["volumeInfo"]["title"])
+#print(decoded[0]["volumeInfo"]["infoLink"])
+#print("")
+
 #print(decoded[1]["volumeInfo"]["title"])
+#print(decoded[1]["volumeInfo"]["infoLink"])
+#print("")
+
 #print(decoded[2]["volumeInfo"]["title"])
+#print(decoded[2]["volumeInfo"]["infoLink"])
+#print("")
+
 #print(decoded[3]["volumeInfo"]["title"])
+#print(decoded[3]["volumeInfo"]["infoLink"])
+#print("")
+# ---------------------------------------------
+
 #print(decoded[4]["volumeInfo"]["title"])
+#print(decoded[4]["volumeInfo"]["infoLink"])
+#print("")
+
+#print(decoded[5]["volumeInfo"]["title"])
+#print(decoded[5]["volumeInfo"]["infoLink"])
+#print("")
+
+#Informacion
+print(decoded[0])
+#print(decoded[1]["volumeInfo"]["publishedDate"])
+#print(decoded[2]["volumeInfo"]["publishedDate"])
+#print(decoded[3]["volumeInfo"]["publishedDate"])
 
 #Enlace al libro
 #print(decoded[0]["volumeInfo"]["infoLink"])
--- mvc/index.py
+++ mvc/index.py
@@ -13,7 +13,14 @@ class Index():
     form = web.input()
     book_name = form.book_name
 
-    result = requests.get("https://www.googleapis.com/books/v1/volumes?q="+book_name)
+  # Libros por nombre
+    #result = requests.get("https://www.googleapis.com/books/v1/volumes?q="+book_name)
+  
+  # Libros disponibles electronicamente    
+    #result = requests.get("https://www.googleapis.com/books/v1/volumes?q="+book_name+"&download=epub")
+
+  # Libros gratuitos y disponibles electronicamente
+    result = requests.get("https://www.googleapis.com/books/v1/volumes?q="+book_name+"&filter=free-ebooks&download=epub")
 
     book = result.json()
     items = book["items"]
@@ -26,10 +33,12 @@ class Index():
     for book in decoded:
       url = book["volumeInfo"]["infoLink"]
       titulo = book["volumeInfo"]["title"]
+      #fechaPublicacion = book["volumeInfo"]["publishedDate"]
 
       datos = {
         "book_name":book_name,
         "titulo":titulo,
+        #"fechaPublicacion":fechaPublicacion,
         "url":url
       }
       books.append(datos)
